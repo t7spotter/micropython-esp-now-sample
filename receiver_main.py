1bb8e8d239e851b879310b20bca77f019b62e12d
Mohamadali Firoozi <alifiroozi@icloud.com>
2024-02-23 23:08:34 +0330

    Added sample main loop for two on and off message

--- receiver_main.py
+++ receiver_main.py
@@ -1,6 +1,7 @@
 import network
 import espnow
 from machine import Pin
+from time import sleep_ms
 
 sta = network.WLAN(network.STA_IF)
 sta.active(True)
@@ -10,3 +11,18 @@ esp = espnow.ESPNow()
 esp.active(True)
 
 led = Pin(18, Pin.OUT)
+
+# main loop
+while True:
+    host, msg_bytes = esp.recv()
+    msg = msg_bytes.decode('utf-8')
+
+    if msg == "ledOn":
+        led.on()
+        print(host, msg)
+
+    elif msg == "ledOff":
+        led.off()
+        print(host, msg)
+
+    sleep_ms(100)
